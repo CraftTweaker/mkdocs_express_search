ffad2c627bbb0eaa83d3e06ce327678a7cd0b9cf
Jared <jaredlll08@gmail.com>
2020-04-03 23:07:21 +0200

    Enable cors

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-var express = require('express');
-var logger = require('morgan');
-var compression = require('compression');
-var helmet = require('helmet');
-var searchRouter = require('./routes/search');
+const express = require('express');
+const logger = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
+const cors = require('cors');
+const searchRouter = require('./routes/search');
 
-var app = express();
+const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(compression());
 app.use(helmet());
+app.use(cors());
 app.use('/', searchRouter);
 
 module.exports = app;
